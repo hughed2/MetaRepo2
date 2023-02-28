22ccac49600881b4fdb02b65e2987d7b8f8884fb
Hughes <david.hughes2@ge.com>
2023-02-28 09:47:10 -0500

    Add Job target

---
+++ MetaTargets/JobTarget.py
@@ -0,0 +1,55 @@
+# This file does a lot of weird things that pylint doesn't like
+# pylint: skip-file
+
+from fastapi import HTTPException
+
+from .MetaTargetBase import MetaTargetBase
+
+class JobTarget(MetaTargetBase):
+    name = 'JobTarget'
+    
+    def validate_target_metadata(self, doc : dict, user_info : dict) -> dict:
+        doc = doc.targetMetadata # We don't need the rest of the document
+
+        # DT4D Target Metadata is entirely supplied by the user. So make sure every key
+        # has been provided, and no extra keys have been provided
+        target_metadata_fields = ['status', 'nativeStatus', 'emitTime', 'receivedTime',
+                                'nativeId', 'parentJobId', 'originJobId',
+                                'computeType']
+        for key in doc:
+            if key not in target_metadata_fields:
+                raise HTTPException(status_code=400, detail="Incorrect targetMetadata field {key}!")
+    
+        # Finalize targetMetadata
+        for key in target_metadata_fields:
+            if key not in doc:
+                raise HTTPException(status_code=400, detail=f"targetMetadata must include a {key}!")
+
+        return doc
+
+    
+    def update_target_metadata(self, doc : dict, update_body: dict,
+                               update_query : dict, archive_format : dict) -> dict:
+        if update_body.targetMetadata is not None:
+            # Make sure targetMetadata doesn't have any fields it's not supposed to    
+            status_fields = ['status', 'nativeStatus', 'emitTime', 'receivedTime']
+            for key in status_fields:
+                if key in update_body.targetMetadata:
+                    for key2 in status_fields:
+                        if key2 not in update_body.targetMetadata:
+                            raise HTTPException(status_code=400, detail=f"If {key} is included, all status fields must be included (status, nativeStatus, emitTime, receivedTime)!")
+    
+            target_metadata_fields = ['nativeId', 'parentJobId', 'originJobId',
+                                'computeType']
+            target_metadata_fields.extend(status_fields)
+            for key in update_body.targetMetadata:
+                if key not in target_metadata_fields:
+                    raise HTTPException(status_code=400, detail=f"Incorrect targetMetadata field {key}!")
+                    
+            update_query["targetMetadata"] = update_body.target_metadata
+            archive_format["previous"] = doc["targetMetadata"]
+            target_metadata_archive = doc["targetMetadataArchive"]
+            target_metadata_archive.append(archive_format)
+            update_query["targetMetadataArchive"] = target_metadata_archive
+
+        return update_query
